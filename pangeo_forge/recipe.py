a42862d87ccd5785280bf619cee1beadbdd91b02
David Brochart <david.brochart@gmail.com>
2021-01-28 23:23:14 +0100

    Specify callable signature
    Co-authored-by: Tom Augspurger <1312546+TomAugspurger@users.noreply.github.com>

--- pangeo_forge/recipe.py
+++ pangeo_forge/recipe.py
@@ -160,7 +160,7 @@ class NetCDFtoZarrSequentialRecipe(BaseRecipe):
     xarray_concat_kwargs: dict = field(default_factory=dict)
     delete_input_encoding: bool = True
     fsspec_open_kwargs: dict = field(default_factory=dict)
-    process_input: Optional[callable] = None
+    process_input: Optional[Callable[[xr.Dataset, str], xr.Dataset] = None
     process_chunk: Optional[callable] = None
 
     def __post_init__(self):
