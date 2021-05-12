82d8cc6b068763be931284832090f637925a2a30
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-05-12 10:45:25 -0700

    pangeo theme installs from pypi

--- docs/conf.py
+++ docs/conf.py
@@ -1,11 +1,8 @@
-# import pangeo_sphinx_book_theme  # noqa
-
 # -- Project information -----------------------------------------------------
 
 project = "Pangeo Forge"
 copyright = "2020, Pangeo Community"
 author = "Pangeo Community"
-
 
 # -- General configuration ---------------------------------------------------
 
@@ -17,7 +14,6 @@ extensions = [
     "sphinx_copybutton",
 ]
 
-# templates_path = ["_templates"]
 exclude_patterns = ["_build", "**.ipynb_checkpoints"]
 master_doc = "index"
 
@@ -29,8 +25,15 @@ execution_excludepatterns = ["tutorials/*"]
 
 # https://sphinx-book-theme.readthedocs.io/en/latest/configure.html
 html_theme = "pangeo_sphinx_book_theme"
-html_theme_options = {}
+html_theme_options = {
+    "repository_url": "https://github.com/pangeo-forge/pangeo-forge-recipes",
+    "repository_branch": "master",
+    "path_to_docs": "docs",
+    "use_repository_button": True,
+    "use_issues_button": True,
+    "use_edit_page_button": True,
+}
 html_logo = "_static/pangeo-forge-logo-blue.png"
 html_static_path = ["_static"]
-html_css_files = ["_static/pangeo-sphinx-book-theme.css",]
+
 myst_heading_anchors = 2
