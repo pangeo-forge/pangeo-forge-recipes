1236adab6a5def2ae1a283ed3729adca1d3c7c1e
cisaacstern <62192187+cisaacstern@users.noreply.github.com>
2021-08-27 10:35:36 -0700

    Skip auth and query string tests in test_fixtures.py

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -1,4 +1,6 @@
 import fsspec
+import pytest
+import requests
 import xarray as xr
 
 from pangeo_forge_recipes.utils import fix_scalar_attr_encoding
@@ -14,6 +16,9 @@ def test_fixture_local_files(daily_xarray_dataset, netcdf_paths):
 
 def test_fixture_http_files(daily_xarray_dataset, netcdf_http_paths):
     urls = netcdf_http_paths[0]
+    r = requests.get(urls[0])
+    if r.status_code == 400 or r.status_code == 401:
+        pytest.skip("Authentication and required query strings are tested in test_storage.py")
     open_files = [fsspec.open(url).open() for url in urls]
     combine = "nested" if len(netcdf_http_paths) == 2 else "by_coords"
     ds = xr.open_mfdataset(open_files, combine=combine, concat_dim="time", engine="h5netcdf").load()
