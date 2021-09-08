e0c4b710e99f3021d6832540cfca54cdd20a81c8
Julius Busecke <julius@ldeo.columbia.edu>
2021-09-07 20:50:09 -0400

    combine tests

--- tests/recipe_tests/test_HDFReferenceRecipe.py
+++ tests/recipe_tests/test_HDFReferenceRecipe.py
@@ -42,46 +42,13 @@ def test_single(netcdf_local_file_pattern_sequential, tmpdir, with_intake):
     assert (ds.foo == expected.foo).all()
 
 
-@pytest.mark.parametrize("with_intake", [True, False])
-def test_multi(netcdf_local_file_pattern_sequential, tmpdir, with_intake):
-    file_pattern = netcdf_local_file_pattern_sequential
-    paths = [f for _, f in list(file_pattern.items())]
-    expected = xr.open_mfdataset(paths, engine="h5netcdf")
-    recipe = HDFReferenceRecipe(file_pattern)
-
-    # make sure assigning storage later works
-    out_target = FSSpecTarget(fs=fsspec.filesystem("file"), root_path=str(tmpdir))
-    metadata_cache = MetadataTarget(fs=fsspec.filesystem("file"), root_path=tempfile.mkdtemp())
-    recipe.target = out_target
-    recipe.metadata_cache = metadata_cache
-
-    recipe.to_dask().compute(scheduler="sync")
-
-    assert out_target.exists("reference.json")
-    assert out_target.exists("reference.yaml")
-
-    if with_intake:
-        cat = intake.open_catalog(out_target._full_path("reference.yaml"))
-        ds = cat.data.read()
-    else:
-        m = fsspec.get_mapper(
-            "reference://",
-            fo=out_target._full_path("reference.json"),
-            target_protocol="file",
-            remote_protocol="file",
-            skip_instance_cache=True,
-        )
-        ds = xr.open_dataset(m, engine="zarr", chunks={}, consolidated=False)
-    assert (ds.foo == expected.foo).all()
-
-
 def _drop_lon(ds):
     return ds.drop_vars(["lon"])
 
 
 @pytest.mark.parametrize("process", [None, _drop_lon])
 @pytest.mark.parametrize("with_intake", [True, False])
-def test_process(netcdf_local_file_pattern_sequential, tmpdir, with_intake, process):
+def test_multi(netcdf_local_file_pattern_sequential, tmpdir, with_intake, process):
     file_pattern = netcdf_local_file_pattern_sequential
     paths = [f for _, f in list(file_pattern.items())]
     expected = xr.open_mfdataset(paths, engine="h5netcdf")
