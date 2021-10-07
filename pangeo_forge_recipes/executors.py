09bf437b24992310ccda2309cc965b4e46e38ce9
Ryan Abernathey <ryan.abernathey@gmail.com>
2021-10-07 16:26:19 -0400

    wip on new executor class

--- pangeo_forge_recipes/executors.py
+++ pangeo_forge_recipes/executors.py
@@ -1,3 +1,28 @@
-from rechunker.executors import DaskPipelineExecutor  # noqa: F401
-from rechunker.executors import PrefectPipelineExecutor  # noqa: F401
-from rechunker.executors import PythonPipelineExecutor  # noqa: F401
+from abc import ABC, abstractmethod
+from dataclasses import dataclass
+from typing import Dict, Callable, Any, Iterable, List, Optional
+from enum import Enum, auto
+
+
+class StageAnnotation(Enum):
+    CONCURRENCY = auto()
+    RETRIES = auto()
+
+
+Config = Any
+
+
+@dataclass(frozen=True)
+class Stage:
+    function: Callable
+    name: str
+    mappable: Optional[Iterable] = None
+    config: Optional[Config] = None
+    annotations: Optional[Dict[StageAnnotation, Any]] = None
+
+
+Pipeline = List[Stage]
+
+
+def pipeline_to_function():
+    pass
