319f6c73f44c5f30410ccfc2e1b125eeb2d3dbff
Ryan Abernathey <ryan.abernathey@gmail.com>
2021-11-08 14:50:58 -0500

    fix incompatibility with xarray 0.20.0

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -8,7 +8,7 @@ from pangeo_forge_recipes.utils import fix_scalar_attr_encoding
 def test_fixture_local_files(daily_xarray_dataset, netcdf_local_paths):
     paths = netcdf_local_paths[0]
     paths = [str(path) for path in paths]
-    ds = xr.open_mfdataset(paths, combine="by_coords", concat_dim="time", engine="h5netcdf")
+    ds = xr.open_mfdataset(paths, combine="by_coords", engine="h5netcdf")
     assert ds.identical(daily_xarray_dataset)
 
 
@@ -19,8 +19,6 @@ def test_fixture_http_files(daily_xarray_dataset, netcdf_http_paths_sequential_1
     if secrets:
         urls = [_add_query_string_secrets(url, secrets) for url in urls]
     open_files = [fsspec.open(url, **open_kwargs).open() for url in urls]
-    ds = xr.open_mfdataset(
-        open_files, combine="by_coords", concat_dim="time", engine="h5netcdf"
-    ).load()
+    ds = xr.open_mfdataset(open_files, combine="by_coords", engine="h5netcdf").load()
     ds = fix_scalar_attr_encoding(ds)  # necessary?
     assert ds.identical(daily_xarray_dataset)
