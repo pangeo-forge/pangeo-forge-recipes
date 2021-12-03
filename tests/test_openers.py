8fe12c1a2708d3f2d13896544193aac409095478
Ryan Abernathey <ryan.abernathey@gmail.com>
2021-12-02 22:38:42 -0500

    big refactor of opener tests

--- tests/test_openers.py
+++ tests/test_openers.py
@@ -3,55 +3,83 @@ from pytest_lazyfixture import lazy_fixture
 
 from pangeo_forge_recipes.openers.fsspec import FsspecLocalCopyOpener, FsspecOpener
 
+EXPECTED_FILE_SIZE = {1: 19000, 2: 29376}  # hard coded expected value
 
-@pytest.mark.parametrize(
-    "file_paths",
-    [
+
+@pytest.fixture(
+    params=[
         lazy_fixture("netcdf_local_paths_sequential"),
         lazy_fixture("netcdf_http_paths_sequential_1d"),
     ],
 )
-@pytest.mark.parametrize("copy_to_local", [False, True])
-@pytest.mark.parametrize("use_cache, cache_first", [(False, False), (True, False), (True, True)])
-@pytest.mark.parametrize("use_xarray", [True, False])
-def test_file_opener(
-    file_paths, tmp_cache, copy_to_local, use_cache, cache_first, use_xarray,
-):
-    all_paths, items_per_file, _, _, kwargs = file_paths
+def path_expected_len_and_kwargs(request):
+    """Take a full set of inputs and return just a single path, plus
+    the other stuff the opener needs to know.
+    """
+
+    all_paths, items_per_file, _, _, kwargs = request.param
+    expected_len = EXPECTED_FILE_SIZE[items_per_file]
     path = str(all_paths[0])
-    fsspec_open_kwargs = kwargs["fsspec_open_kwargs"]
-    secrets = kwargs["query_string_secrets"]
+    return path, expected_len, kwargs
 
+
+def _cache_checker(opener, cache, path):
+    """Check that the cache on an opener is working."""
+
+    opener.cache_file(path)
+    assert cache.exists(path)
+    details = cache.fs.ls(cache.root_path, detail=True)
+    opener.cache_file(path)
+    # check that nothing happened
+    assert cache.fs.ls(cache.root_path, detail=True) == details
+
+
+def _passthrough(opener, cache, path):
+    pass
+
+
+@pytest.fixture(params=[(True, _cache_checker), (False, _passthrough)])
+def cache_and_cache_checker(request, tmp_cache):
+    use_cache, checker = request.param
     cache = tmp_cache if use_cache else None
+    return cache, checker
 
-    if copy_to_local:
-        Opener = FsspecLocalCopyOpener
-    else:
-        Opener = FsspecOpener
-    opener = Opener(cache=cache, secrets=secrets, fsspec_open_kwargs=fsspec_open_kwargs)
 
-    if cache_first:
-        opener.cache_file(path)
-        assert cache.exists(path)
-        details = cache.fs.ls(cache.root_path, detail=True)
-        opener.cache_file(path)
-        # check that nothing happened
-        assert cache.fs.ls(cache.root_path, detail=True) == details
-
-    if use_cache and not cache_first:
-        with pytest.raises(FileNotFoundError):
-            with opener.open(path) as fp:
-                pass
-        return
+def test_fsspec_opener(path_expected_len_and_kwargs, cache_and_cache_checker):
+    path, expected_len, kwargs = path_expected_len_and_kwargs
+    cache, cache_checker = cache_and_cache_checker
+    opener = FsspecOpener(
+        cache=cache,
+        secrets=kwargs["query_string_secrets"],
+        fsspec_open_kwargs=kwargs["fsspec_open_kwargs"],
+    )
+    cache_checker(opener, cache, path)
 
     with opener.open(path) as fp:
-        if copy_to_local:
-            assert isinstance(fp, str)
-            with open(fp, mode="rb") as fp2:
-                data = fp2.read()
-        else:
-            data = fp.read()
-            assert hasattr(fp, "fs")  # should be true for fsspec.OpenFile objects
+        data = fp.read()
+        assert hasattr(fp, "fs")  # should be true for fsspec.OpenFile objects
+    assert len(data) == expected_len
 
-    expected_length = {1: 19000, 2: 29376}  # hard coded expected value
-    assert len(data) == expected_length[items_per_file]
+
+def test_fsspec_local_copy_opener(path_expected_len_and_kwargs, cache_and_cache_checker):
+    path, expected_len, kwargs = path_expected_len_and_kwargs
+    cache, cache_checker = cache_and_cache_checker
+    opener = FsspecLocalCopyOpener(
+        cache=cache,
+        secrets=kwargs["query_string_secrets"],
+        fsspec_open_kwargs=kwargs["fsspec_open_kwargs"],
+    )
+    cache_checker(opener, cache, path)
+
+    with opener.open(path) as fp:
+        assert isinstance(fp, str)
+        with open(fp, mode="rb") as fp2:
+            data = fp2.read()
+    assert len(data) == expected_len
+
+
+#     if use_cache and not cache_first:
+#         with pytest.raises(FileNotFoundError):
+#             with opener.open(path) as fp:
+#                 pass
+#         return
