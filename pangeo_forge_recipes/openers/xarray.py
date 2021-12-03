3cb46e3b4eff7a932f618a807ff421302d096cd2
Ryan Abernathey <ryan.abernathey@gmail.com>
2021-12-02 23:31:33 -0500

    add xarray opener and test

---
+++ pangeo_forge_recipes/openers/xarray.py
@@ -0,0 +1,100 @@
+import hashlib
+import logging
+from contextlib import contextmanager
+from dataclasses import dataclass, field
+from typing import Generator, Optional
+
+import fsspec
+import xarray as xr
+
+from ..reference import create_hdf5_reference, unstrip_protocol
+from ..storage import MetadataTarget
+from .base import BaseOpener
+from .fsspec import FsspecLocalCopyOpener, FsspecOpener
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class XarrayOpener(BaseOpener[str, xr.Dataset]):
+    """Open an input as an Xarray Dataset directly from a url path."""
+
+    xarray_open_kwargs: dict = field(default_factory=dict)
+    load: bool = False
+
+    @contextmanager
+    def open(self, input: str) -> Generator[xr.Dataset, None, None]:
+        kw = self.xarray_open_kwargs.copy()
+        if "engine" not in kw:
+            kw["engine"] = "h5netcdf"
+        with xr.open_dataset(input, **kw) as ds:
+            if self.load:
+                ds.load()
+            yield ds
+
+
+@dataclass(frozen=True)
+class XarrayFsspecOpener(XarrayOpener, FsspecOpener, BaseOpener[str, xr.Dataset]):
+    """Open an input as an Xarray Dataset via fsspec."""
+
+    @contextmanager
+    def open(self, input: str) -> Generator[xr.Dataset, None, None]:
+        with super(FsspecOpener, self).open(input) as f_obj:
+            with super(XarrayOpener, self).open(f_obj) as ds:
+                yield ds
+
+
+@dataclass(frozen=True)
+class XarrayFsspecLocalCopyOpener(XarrayOpener, FsspecLocalCopyOpener, BaseOpener[str, xr.Dataset]):
+    """Open an input as an xarray Dataset via fsspec,
+    first copying the input to a local temporary file.
+    """
+
+    @contextmanager
+    def open(self, input: str) -> Generator[xr.Dataset, None, None]:
+        with super(FsspecLocalCopyOpener, self).open(input) as path:
+            with super(XarrayOpener, self).open(path) as ds:
+                yield ds
+
+
+def _input_reference_fname(input: str) -> str:
+    hash = hashlib.md5(input.encode()).hexdigest()
+    return f"input-reference-{hash}.json"
+
+
+# not sure if frozen is a good choice--we might want to dynamically set metadata_cache
+@dataclass(frozen=True)
+class XarrayFsspecReferenceOpener(XarrayOpener, FsspecOpener, BaseOpener[str, xr.Dataset]):
+    """Open an input as an Xarray Dataset via fsspec."""
+
+    metadata_cache: Optional[MetadataTarget] = None
+
+    def cache_input_metadata(self, input: str) -> None:
+        if self.cache is None:
+            protocol = fsspec.utils.get_protocol(input)
+            url = unstrip_protocol(input, protocol)
+        else:
+            url = unstrip_protocol(self.cache._full_path(input), self.cache.fs.protocol)
+        with super(FsspecOpener, self).open(input) as fp:
+            ref_data = create_hdf5_reference(fp, url, input)
+        ref_fname = _input_reference_fname(input)
+        self.metadata_cache[ref_fname] = ref_data
+
+    @contextmanager
+    def open(self, input: str) -> Generator[xr.Dataset, None, None]:
+        from fsspec.implementations.reference import ReferenceFileSystem
+
+        reference_data = self.metadata_cache[_input_reference_fname(input)]
+
+        # TODO: figure out how to set this for the cache target
+        remote_protocol = fsspec.utils.get_protocol(input)
+        ref_fs = ReferenceFileSystem(
+            reference_data, remote_protocol=remote_protocol, skip_instance_cache=True
+        )
+        mapper = ref_fs.get_mapper("/")
+
+        # Doesn't really need to be a context manager, but that's how this function works
+        with xr.open_dataset(
+            mapper, engine="zarr", chunks={}, consolidated=False, **self.xarray_open_kwargs
+        ) as ds:
+            yield ds
--- tests/test_openers.py
+++ tests/test_openers.py
@@ -1,28 +1,43 @@
 import os
 
 import pytest
+import xarray as xr
 from pytest_lazyfixture import lazy_fixture
 
 from pangeo_forge_recipes.openers.fsspec import FsspecLocalCopyOpener, FsspecOpener
+from pangeo_forge_recipes.openers.xarray import XarrayOpener
 
 EXPECTED_FILE_SIZE = {1: 19000, 2: 29376}  # hard coded expected value
 
 
-@pytest.fixture(
-    params=[
-        lazy_fixture("netcdf_local_paths_sequential"),
-        lazy_fixture("netcdf_http_paths_sequential_1d"),
-    ],
-)
-def path_expected_size_and_kwargs(request):
+def _make_path_expected_size_and_kwargs(paths_fixture):
     """Take a full set of inputs and return just a single path, plus
     the other stuff the opener needs to know.
     """
-
-    all_paths, items_per_file, _, _, kwargs = request.param
+    all_paths, items_per_file, _, _, kwargs = paths_fixture
     expected_size = EXPECTED_FILE_SIZE[items_per_file]
     path = str(all_paths[0])
     return path, expected_size, kwargs
+
+
+@pytest.fixture
+def netcdf_local_path_expected_size_and_kwargs(netcdf_local_paths_sequential):
+    return _make_path_expected_size_and_kwargs(netcdf_local_paths_sequential)
+
+
+@pytest.fixture
+def netcdf_http_path_expected_size_and_kwargs(netcdf_http_paths_sequential_1d):
+    return _make_path_expected_size_and_kwargs(netcdf_http_paths_sequential_1d)
+
+
+@pytest.fixture(
+    params=[
+        lazy_fixture("netcdf_local_path_expected_size_and_kwargs"),
+        lazy_fixture("netcdf_http_path_expected_size_and_kwargs"),
+    ],
+)
+def netcdf_path_expected_size_and_kwargs(request):
+    return request.param
 
 
 def _cache_checker(opener, cache, path):
@@ -47,8 +62,8 @@ def cache_and_cache_checker(request, tmp_cache):
     return cache, checker
 
 
-def test_fsspec_opener(path_expected_size_and_kwargs, cache_and_cache_checker):
-    path, expected_size, kwargs = path_expected_size_and_kwargs
+def test_fsspec_opener(netcdf_path_expected_size_and_kwargs, cache_and_cache_checker):
+    path, expected_size, kwargs = netcdf_path_expected_size_and_kwargs
     cache, cache_checker = cache_and_cache_checker
     opener = FsspecOpener(
         cache=cache,
@@ -63,8 +78,8 @@ def test_fsspec_opener(path_expected_size_and_kwargs, cache_and_cache_checker):
     assert len(data) == expected_size
 
 
-def test_fsspec_local_copy_opener(path_expected_size_and_kwargs, cache_and_cache_checker):
-    path, expected_size, kwargs = path_expected_size_and_kwargs
+def test_fsspec_local_copy_opener(netcdf_path_expected_size_and_kwargs, cache_and_cache_checker):
+    path, expected_size, kwargs = netcdf_path_expected_size_and_kwargs
     cache, cache_checker = cache_and_cache_checker
     opener = FsspecLocalCopyOpener(
         cache=cache,
@@ -79,6 +94,14 @@ def test_fsspec_local_copy_opener(path_expected_size_and_kwargs, cache_and_cache
             data = fp2.read()
     assert not os.path.exists(fname)  # make sure file got cleaned up
     assert len(data) == expected_size
+
+
+def test_xarray_opener(netcdf_local_path_expected_size_and_kwargs):
+    path, _, _ = netcdf_local_path_expected_size_and_kwargs
+    opener = XarrayOpener()
+    print(path, type(path))
+    with opener.open(path) as ds:
+        assert isinstance(ds, xr.Dataset)
 
 
 #     if use_cache and not cache_first:
