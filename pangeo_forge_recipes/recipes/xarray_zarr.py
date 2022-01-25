90bf4f35b1fb674dce5c2f4f81ea4e10fb7b2824
Alex Merose <alxr@google.com>
2022-01-24 16:52:13 -0800

    Update comment.

--- pangeo_forge_recipes/recipes/xarray_zarr.py
+++ pangeo_forge_recipes/recipes/xarray_zarr.py
@@ -553,7 +553,7 @@ def finalize_target(*, config: XarrayZarrRecipe) -> None:
         target_mapper = config.target.get_mapper()
         group = zarr.open(target_mapper, mode="a")
         # https://github.com/pangeo-forge/pangeo-forge-recipes/issues/214
-        # intersect the dims from the array metadata with the Zarr group
+        # filter out the dims from the array metadata not in the Zarr group
         # to handle coordinateless dimensions.
         dims = (dim for dim in _gather_coordinate_dimensions(group) if dim in group)
         for dim in dims:
