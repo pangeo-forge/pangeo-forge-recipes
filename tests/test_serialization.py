50b522d00e6af58dc0307a2b454c44a6b3b1fa2e
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2022-04-27 14:43:46 -0700

    in pattern.sha256 root, add nitems_per_file, remove is_opendap

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -12,11 +12,11 @@ URL_FORMAT = (
 )
 
 
-def make_file_pattern(dates):
+def make_file_pattern(dates, nitems_per_file):
     def make_url(time):
         return URL_FORMAT.format(time=time)
 
-    time_concat_dim = ConcatDim("time", dates, nitems_per_file=1)
+    time_concat_dim = ConcatDim("time", dates, nitems_per_file=nitems_per_file)
     pattern = FilePattern(make_url, time_concat_dim)
 
     return pattern
@@ -28,15 +28,13 @@ def end_date():
 
 
 @pytest.fixture
-def old_pattern_with_last_hash(end_date):
+def old_pattern_sha256(end_date):
     dates = pd.date_range("1981-09-01", end_date, freq="D")
-    pattern = make_file_pattern(dates)
-    last_hash = pattern.sha256()
-    return pattern, last_hash
+    pattern = make_file_pattern(dates, nitems_per_file=1)
+    return pattern.sha256()
 
 
-@pytest.fixture
-def new_pattern_with_next_url(end_date):
+def get_new_pattern_with_next_url(end_date, nitems_per_file):
 
     fmt = "%Y-%m-%d"
 
@@ -46,12 +44,20 @@ def new_pattern_with_next_url(end_date):
     next_day = increment_end_date(ndays=1)
     new_end_date = increment_end_date(ndays=90).strftime(fmt)
     new_dates = pd.date_range("1981-09-01", new_end_date, freq="D")
-    new_pattern = make_file_pattern(new_dates)
+    new_pattern = make_file_pattern(new_dates, nitems_per_file=nitems_per_file)
     return new_pattern, URL_FORMAT.format(time=next_day)
 
 
-def test_match_pattern_blockchain(old_pattern_with_last_hash, new_pattern_with_next_url):
-    _, last_hash = old_pattern_with_last_hash
-    new_pattern, next_url = new_pattern_with_next_url
-    matching_key = match_pattern_blockchain(last_hash, new_pattern)
-    assert new_pattern[matching_key] == next_url
+@pytest.mark.parametrize("new_pattern_nitems_per_file", [1, 2])
+def test_match_pattern_blockchain(
+    old_pattern_sha256,
+    end_date,
+    new_pattern_nitems_per_file,
+):
+    new_pattern, next_url = get_new_pattern_with_next_url(end_date, new_pattern_nitems_per_file)
+    matching_key = match_pattern_blockchain(old_pattern_sha256, new_pattern)
+
+    if new_pattern_nitems_per_file == 1:
+        assert new_pattern[matching_key] == next_url
+    elif new_pattern_nitems_per_file == 2:
+        assert matching_key is None
