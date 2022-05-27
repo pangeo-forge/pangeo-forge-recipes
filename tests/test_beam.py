0f7f0aa049f7d34c63ad45cdb41d704ca336dba6
Ryan Abernathey <rpa@ldeo.columbia.edu>
2022-05-27 15:41:53 -0400

    add more testing to test_OpenWithFSSpec

--- tests/test_beam.py
+++ tests/test_beam.py
@@ -8,16 +8,6 @@ from apache_beam.testing.util import BeamAssertException, assert_that, is_not_em
 from pangeo_forge_recipes.transforms import OpenWithFSSpec
 
 
-# a beam testing assertion matcher
-def expected_len(n):
-    def _expected_len(actual):
-        actual_len = len(actual)
-        if actual_len != n:
-            raise BeamAssertException(f"Failed assert: actual len is {actual_len}, expected {n}")
-
-    return _expected_len
-
-
 @pytest.fixture
 def pcoll_opened_files(netcdf_local_file_pattern_sequential):
     pattern = netcdf_local_file_pattern_sequential
@@ -26,8 +16,28 @@ def pcoll_opened_files(netcdf_local_file_pattern_sequential):
 
 def test_OpenWithFSSpec(pcoll_opened_files):
     pattern, pcoll = pcoll_opened_files
+
+    def expected_len(n):
+        def _expected_len(actual):
+            actual_len = len(actual)
+            if actual_len != n:
+                raise BeamAssertException(
+                    f"Failed assert: actual len is {actual_len}, expected {n}"
+                )
+
+        return _expected_len
+
+    def is_readable():
+        def _is_readable(actual):
+            for index, item in actual:
+                with item as fp:
+                    _ = fp.read()
+
+        return _is_readable
+
     with TestPipeline() as p:
         output = p | pcoll
 
         assert_that(output, is_not_empty(), label="ouputs not empty")
         assert_that(output, expected_len(pattern.shape[0]), label="expected len")
+        assert_that(output, is_readable(), label="output is readable")
