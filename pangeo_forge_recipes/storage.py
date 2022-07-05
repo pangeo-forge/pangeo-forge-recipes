9b8145e539632cabdbfdc34ff95c2ec34f2f6778
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-07-05 15:51:22 -0400

    replace generic mapper with FSStore

--- pangeo_forge_recipes/storage.py
+++ pangeo_forge_recipes/storage.py
@@ -17,6 +17,7 @@ from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
 
 import fsspec
 from fsspec.implementations.local import LocalFileSystem
+from zarr.storage import FSStore
 
 logger = logging.getLogger(__name__)
 
@@ -101,7 +102,7 @@ class FSSpecTarget(AbstractTarget):
 
     def get_mapper(self) -> fsspec.mapping.FSMap:
         """Get a mutable mapping object suitable for storing Zarr data."""
-        return self.fs.get_mapper(self.root_path)
+        return FSStore(self.root_path, fs=self.fs)
 
     def _full_path(self, path: str):
         return os.path.join(self.root_path, path)
