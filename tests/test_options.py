3d1b15986c1fc958084369b603c32aedea6de724
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-07-05 12:06:27 -0400

    try to use options

--- tests/test_options.py
+++ tests/test_options.py
@@ -1,6 +1,7 @@
+from dataclasses import dataclass
+
 import apache_beam as beam
 from apache_beam.options.pipeline_options import PipelineOptions
-from apache_beam.options.value_provider import RuntimeValueProvider
 from apache_beam.testing.test_pipeline import TestPipeline
 from apache_beam.testing.util import assert_that
 
@@ -8,10 +9,16 @@ from pangeo_forge_recipes.options import PangeoForgeOptions
 
 
 def test_PangeoForgeOptions():
-    def get_options(item):
-        target_url = RuntimeValueProvider.get_value("target_url", str, "")
-        cache_url = RuntimeValueProvider.get_value("cache_url", str, "")
-        return {"target_url": target_url, "cache_url": cache_url}
+    # This test is basically just a demonstration of how we can set pipeline options.
+    # It is based on https://beam.apache.org/documentation/patterns/pipeline-options/
+
+    @dataclass
+    class GetPangeoForgeOptions(beam.DoFn):
+        target_url: str
+        cache_url: str
+
+        def process(self, item):
+            yield {"target_url": self.target_url, "cache_url": self.cache_url}
 
     def has_correct_options():
         def _check_results(actual):
@@ -22,8 +29,12 @@ def test_PangeoForgeOptions():
         return _check_results
 
     pipeline_options = PipelineOptions(target_url="foo", cache_url="bar")
-    custom_options = pipeline_options.view_as(PangeoForgeOptions)
+    pf_options = pipeline_options.view_as(PangeoForgeOptions)
 
-    with TestPipeline(options=custom_options) as p:
-        pcol = p | beam.Create([None]) | beam.Map(get_options)
+    with TestPipeline(options=pipeline_options) as p:
+        pcol = (
+            p
+            | beam.Create([None])
+            | beam.ParDo(GetPangeoForgeOptions(pf_options.target_url, pf_options.cache_url))
+        )
         assert_that(pcol, has_correct_options())
