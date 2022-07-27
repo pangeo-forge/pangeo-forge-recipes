b57c9ba8da9cdeffc0b0b6396dfc095de75aa85f
Ryan Abernathey <ryan.abernathey@gmail.com>
2022-07-26 22:53:59 -0400

    end-to-end test

--- pangeo_forge_recipes/transforms.py
+++ pangeo_forge_recipes/transforms.py
@@ -8,11 +8,12 @@ from typing import Dict, List, Optional, Tuple, TypeVar
 
 import apache_beam as beam
 
-from .aggregation import schema_to_zarr
+from .aggregation import XarraySchema, dataset_to_schema, schema_to_zarr
 from .combiners import CombineXarraySchemas
 from .openers import open_url, open_with_xarray
-from .patterns import DimKey, FileType, Index
+from .patterns import CombineOp, DimKey, FileType, Index, augment_index_with_start_stop
 from .storage import CacheFSSpecTarget, FSSpecTarget
+from .writers import store_dataset_fragment
 
 logger = logging.getLogger(__name__)
 
@@ -145,6 +146,12 @@ class _NestDim(beam.PTransform):
 
 
 @dataclass
+class DatasetToSchema(beam.PTransform):
+    def expand(self, pcoll: beam.PCollection) -> beam.PCollection:
+        return pcoll | beam.Map(_add_keys(dataset_to_schema))
+
+
+@dataclass
 class DetermineSchema(beam.PTransform):
     """Combine many Dataset schemas into a single schema along multiple dimensions.
     This is a reduction that produces a singleton PCollection.
@@ -166,6 +173,28 @@ class DetermineSchema(beam.PTransform):
                     pcoll | _NestDim(last_dim) | beam.CombinePerKey(CombineXarraySchemas(last_dim))
                 )
         return pcoll
+
+
+@dataclass
+class IndexItems(beam.PTransform):
+    """Augment dataset indexes with information about start and stop position."""
+
+    schema: beam.PCollection
+
+    @staticmethod
+    def index_item(item: Indexed[T], schema: XarraySchema) -> Indexed[T]:
+        index, ds = item
+        new_index = Index()
+        for dimkey, dimval in index.items():
+            if dimkey.operation == CombineOp.CONCAT:
+                item_len_dict = schema["chunks"][dimkey.name]
+                item_lens = [item_len_dict[n] for n in range(len(item_len_dict))]
+                dimval = augment_index_with_start_stop(dimval, item_lens)
+            new_index[dimkey] = dimval
+        return new_index, ds
+
+    def expand(self, pcoll: beam.PCollection):
+        return pcoll | beam.Map(self.index_item, schema=beam.pvalue.AsSingleton(self.schema))
 
 
 @dataclass
@@ -191,3 +220,14 @@ class PrepareZarrTarget(beam.PTransform):
             schema_to_zarr, target_store=store, target_chunks=self.target_chunks
         )
         return initialized_target
+
+
+@dataclass
+class StoreToZarr(beam.PTransform):
+
+    target_store: beam.PCollection  # side input
+
+    def expand(self, pcoll: beam.PCollection):
+        return pcoll | beam.Map(
+            store_dataset_fragment, target_store=beam.pvalue.AsSingleton(self.target_store)
+        )
--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -4,22 +4,30 @@ import xarray as xr
 from apache_beam.options.pipeline_options import PipelineOptions
 from apache_beam.testing.test_pipeline import TestPipeline
 
+from pangeo_forge_recipes.patterns import CombineOp, DimKey
+from pangeo_forge_recipes.transforms import (
+    DatasetToSchema,
+    DetermineSchema,
+    IndexItems,
+    OpenWithXarray,
+    PrepareZarrTarget,
+    StoreToZarr,
+)
+
 # from apache_beam.testing.util import assert_that, equal_to
 # from apache_beam.testing.util import BeamAssertException, assert_that, is_not_empty
 
-from pangeo_forge_recipes.patterns import DimKey, CombineOp
-from pangeo_forge_recipes.transforms import (
-    OpenWithXarray, DatasetToSchema, PrepareZarrTarget,
-    DetermineSchema, StoreToZarr, IndexItems
-)
 
 @pytest.fixture
 def pipeline():
-    options = PipelineOptions(runtime_type_check=True)
+    options = PipelineOptions(runtime_type_check=False)
     with TestPipeline(options=options) as p:
         yield p
 
-def test_xarray_zarr(daily_xarray_dataset, netcdf_local_file_pattern_sequential, pipeline, tmp_target_url):
+
+def test_xarray_zarr(
+    daily_xarray_dataset, netcdf_local_file_pattern_sequential, pipeline, tmp_target_url
+):
     pattern = netcdf_local_file_pattern_sequential
     with pipeline as p:
         inputs = p | beam.Create(pattern.items())
