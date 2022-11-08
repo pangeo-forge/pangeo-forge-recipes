599727c56e605098bfa90b5ef2826501004ab739
Raphael Hagen <norlandrhagen@gmail.com>
2022-11-08 15:54:04 -0800

    minor change to test_serialization

--- tests/test_serialization.py
+++ tests/test_serialization.py
@@ -7,8 +7,8 @@ import pytest
 from fsspec.implementations.local import LocalFileSystem
 
 from pangeo_forge_recipes.patterns import ConcatDim, FilePattern, FileType, match_pattern_blockchain
-from pangeo_forge_recipes.recipes import BaseRecipe, XarrayZarrRecipe
-from pangeo_forge_recipes.recipes.reference_zarr import GribReferenceRecipe, HDFReferenceRecipe
+from pangeo_forge_recipes.recipes import XarrayZarrRecipe
+from pangeo_forge_recipes.recipes.reference_zarr import HDFReferenceRecipe
 from pangeo_forge_recipes.serialization import dict_to_sha256, either_encode_or_hash
 from pangeo_forge_recipes.storage import FSSpecTarget, StorageConfig
 
