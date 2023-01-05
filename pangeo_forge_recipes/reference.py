ad293d8856bf3d7a38604546fa97d613cdfacfaa
Raphael Hagen <norlandrhagen@gmail.com>
2023-01-04 16:12:09 -0800

    successfully created grib2 ref recipe

--- pangeo_forge_recipes/reference.py
+++ pangeo_forge_recipes/reference.py
@@ -18,14 +18,37 @@ def create_kerchunk_reference(
     inline_threshold: int = 300,
     grib_filters: Optional[dict] = None,
 ) -> Dict:
+
     if file_type == FileType.netcdf4:
         chunks = SingleHdf5ToZarr(fp, url, inline_threshold=inline_threshold)
+        ref = chunks.translate()
+
     elif file_type == FileType.netcdf3:
         chunks = NetCDF3ToZarr(url, max_chunk_size=100_000_000)
+        ref = chunks.translate()
     elif file_type == FileType.grib:
-        chunks = scan_grib(fp, inline_threshold=inline_threshold, filter=grib_filters)
 
-    return chunks.translate()
+        grib_references = scan_grib(
+            url,
+            inline_threshold=inline_threshold,
+            filter=grib_filters,
+        )
+
+        # Consolidate / post-process references
+        if len(grib_references) == 1:
+            ref = grib_references[0]
+            ref["templates"] = {"u": url}
+            return ref
+
+        ref = grib_references[0].copy()
+        ref["templates"] = {"u": url}
+
+        primary_refs = ref["refs"].copy()
+        for i, other_ref in enumerate(grib_references[1:]):
+            primary_refs.update(other_ref["refs"])
+        ref["refs"] = primary_refs
+
+    return ref
 
 
 def unstrip_protocol(name: str, protocol: Union[str, Tuple[str, ...]]) -> str:
