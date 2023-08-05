11de660bb9a3b18cacd7df61034a73cc0371071e
Julius Busecke <julius@ldeo.columbia.edu>
2023-08-05 12:29:59 +0200

    Update transforms.py

--- pangeo_forge_recipes/transforms.py
+++ pangeo_forge_recipes/transforms.py
@@ -448,7 +448,7 @@ class StoreToZarr(beam.PTransform, ZarrWriterMixin):
                 target_chunks_aspect_ratio=cast(Dict[str, int], self.target_chunks_aspect_ratio),
                 size_tolerance=self.size_tolerance,
             )
-            print(f"Dynamically determined target_chunks: {target_chunks}")
+            logger.info(f"Dynamically determined target_chunks: {target_chunks}")
         else:
             target_chunks = cast(Dict[str, int], self.target_chunks)
         return target_chunks
