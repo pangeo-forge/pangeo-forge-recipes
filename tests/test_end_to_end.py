86cb403940121025954d1cc1fe8f828b7f21e8e7
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-09-07 16:57:12 -0700

    fix xfail marker

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -1,3 +1,4 @@
+import importlib.util
 import os
 from pathlib import Path
 
@@ -106,9 +107,13 @@ def test_reference_netcdf(
 
 
 @pytest.mark.xfail(
-    "Requires cfgrib, which should be installed via conda. "
-    "FIXME: Setup separate testing environment for `requires-conda` tests. "
-    "NOTE: The HRRR integration tests would also fall into this category."
+    importlib.util.find_spec("cfgrib") is None,
+    reason=(
+        "Requires cfgrib, which should be installed via conda. "
+        "FIXME: Setup separate testing environment for `requires-conda` tests. "
+        "NOTE: The HRRR integration tests would also fall into this category."
+    ),
+    raises=ImportError,
 )
 def test_reference_grib(
     pipeline,
