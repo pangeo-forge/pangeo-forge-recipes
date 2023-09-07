694c2ed3fd8ac9a257001154595324f31870369f
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-09-07 16:04:13 -0700

    xfail grib end-to-end test

--- tests/test_end_to_end.py
+++ tests/test_end_to_end.py
@@ -105,6 +105,11 @@ def test_reference_netcdf(
     xr.testing.assert_equal(ds.load(), daily_xarray_dataset)
 
 
+@pytest.mark.xfail(
+    "Requires cfgrib, which should be installed via conda. "
+    "FIXME: Setup separate testing environment for `requires-conda` tests. "
+    "NOTE: The HRRR integration tests would also fall into this category."
+)
 def test_reference_grib(
     pipeline,
     tmp_target_url,
