50618c5ac749b2d12e6e6543d41df15911405718
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2023-10-03 14:59:46 -0400

    update value-spec names

--- pangeo_forge_recipes/injections.py
+++ pangeo_forge_recipes/injections.py
@@ -1,10 +1,10 @@
 def get_injection_specs():
     return {
         "StoreToZarr": {
-            "target_root": "OUTPUT_ROOT",
+            "target_root": "TARGET_STORAGE",
         },
         "WriteCombinedReference": {
-            "target_root": "OUTPUT_ROOT",
+            "target_root": "TARGET_STORAGE",
         },
-        "OpenURLWithFSSpec": {"cache": "CACHE_ROOT"},
+        "OpenURLWithFSSpec": {"cache": "INPUT_CACHE_STORAGE"},
     }
