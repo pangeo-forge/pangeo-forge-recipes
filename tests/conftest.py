116279c56da985e5ad4c0900feb48f84d63d3fa6
Raphael Hagen <norlandrhagen@gmail.com>
2023-10-12 15:57:53 -0700

    direct copy of minio infra from pangeo-forge-runner

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,8 +14,11 @@ Note:
    Recipe fixtures are defined in their respective test modules, e.g. `test_recipes.py`
 """
 import os
+import secrets
+import signal
 import socket
 import subprocess
+import tempfile
 import time
 
 import aiohttp
@@ -506,3 +509,54 @@ def tmp_cache(tmpdir_factory):
 def tmp_cache_url(tmpdir_factory):
     path = str(tmpdir_factory.mktemp("cache"))
     return path
+
+
+# local_ip and minio copied directly from pangeo-forge-runner conftest.py
+@pytest.fixture(scope="session")
+def local_ip():
+    """
+    Return IP of current machine
+
+    Hopefully, this is resolveable by both code running on the machine
+    as well as whatever kubernetes cluster is being used to run tests.
+    """
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(("10.254.254.254", 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = "127.0.0.1"
+    finally:
+        s.close()
+    return IP
+
+
+@pytest.fixture(scope="session")
+def minio(local_ip):
+    """
+    Start a temporary minio instance & return values used to connect to it
+
+    yields a dictionary with the following keys:
+    - endpoint - the HTTP endpoint to use to talk to the minio API
+    - username - the minio root username (aka 'key' in S3 parlance)
+    - password - the minio root password
+    """
+    username = secrets.token_hex(16)
+    password = secrets.token_hex(16)
+    address = f"{local_ip}:19555"
+    endpoint = f"http://{address}"
+
+    env = os.environ.copy()
+    env.update({"MINIO_ROOT_USER": username, "MINIO_ROOT_PASSWORD": password})
+    with tempfile.TemporaryDirectory() as d:
+        proc = subprocess.Popen(["minio", "server", d, "--address", address], env=env)
+
+        yield {"endpoint": endpoint, "username": username, "password": password}
+
+        # Cleanup minio server during teardown
+        proc.send_signal(signal.SIGTERM)
+        proc.wait()
+
+        assert proc.returncode == 0
