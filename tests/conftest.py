fa2b8b1dafde69fccd6e7f5f050e3860c014ad80
Raphael Hagen <norlandrhagen@gmail.com>
2023-10-31 13:51:54 -0600

    Updated pyproj toml to split out docker dev into minio specific extra + updated ci/integration

--- tests/conftest.py
+++ tests/conftest.py
@@ -15,10 +15,8 @@ Note:
 """
 import os
 import secrets
-import signal
 import socket
 import subprocess
-import tempfile
 import time
 
 import aiohttp
@@ -511,58 +509,10 @@ def tmp_cache_url(tmpdir_factory):
     return path
 
 
-# # local_ip and minio copied directly from pangeo-forge-runner conftest.py
-# @pytest.fixture(scope="session")
-# def local_ip():
-#     """
-#     Return IP of current machine
-#
-#     Hopefully, this is resolveable by both code running on the machine
-#     as well as whatever kubernetes cluster is being used to run tests.
-#     """
-#     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-#     s.settimeout(0)
-#     try:
-#         # doesn't even have to be reachable
-#         s.connect(("10.254.254.254", 1))
-#         IP = s.getsockname()[0]
-#     except Exception:
-#         IP = "127.0.0.1"
-#     finally:
-#         s.close()
-#     return IP
-
-
-# @pytest.fixture(scope="session")
-# def minio(local_ip):
-#     """
-#     Start a temporary minio instance & return values used to connect to it
-#
-#     yields a dictionary with the following keys:
-#     - endpoint - the HTTP endpoint to use to talk to the minio API
-#     - username - the minio root username (aka 'key' in S3 parlance)
-#     - password - the minio root password
-#     """
-#     username = secrets.token_hex(16)
-#     password = secrets.token_hex(16)
-#     address = f"{local_ip}:19555"
-#     endpoint = f"http://{address}"
-#
-#     env = os.environ.copy()
-#     env.update({"MINIO_ROOT_USER": username, "MINIO_ROOT_PASSWORD": password})
-#     with tempfile.TemporaryDirectory() as d:
-#         proc = subprocess.Popen(["minio", "server", d, "--address", address], env=env)
-#         yield {"endpoint": endpoint, "username": username, "password": password}
-#
-#         # Cleanup minio server during teardown
-#         proc.send_signal(signal.SIGTERM)
-#         proc.wait()
-#
-#         assert proc.returncode == 0
-
 @pytest.fixture(scope="session")
 def minio():
     import docker
+
     client = docker.from_env()
     port = 9000
     username = secrets.token_hex(16)
@@ -571,11 +521,11 @@ def minio():
         "minio/minio",
         "server /data",
         detach=True,
-        ports={f'{port}/tcp': port},
+        ports={f"{port}/tcp": port},
         environment={
-            'MINIO_ACCESS_KEY': username,
-            'MINIO_SECRET_KEY': password,
-        }
+            "MINIO_ACCESS_KEY": username,
+            "MINIO_SECRET_KEY": password,
+        },
     )
     time.sleep(10)  # give it time to boot
     # enter
