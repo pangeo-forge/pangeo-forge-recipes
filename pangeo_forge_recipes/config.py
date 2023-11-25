323a6c364a9196bc1d175309134867ff10eebfab
ranchodeluxe <greg.corradini@gmail.com>
2023-11-25 06:35:56 -0800

    change docstring to match feedback

--- pangeo_forge_recipes/config.py
+++ pangeo_forge_recipes/config.py
@@ -7,32 +7,7 @@ from pangeo_forge_recipes.transforms import RequiredAtRuntimeDefault
 
 @dataclass
 class Config:
-    """a simple class that contains all possible injection spec values
-
-    folks can import it into their recipes to pass injection spec values
-    around to custom beam routines as the example below shows
-
-    Examples:
-        ```python
-        from config import Config
-
-        @dataclass
-        class MyCustomTransform(beam.PTransform):
-
-            # this custom transform class is not listed
-            # in `pangeo_forge_recipes.injections.py:get_injection_specs()`
-            # and the instance attr will therefore not be dependency injected
-            # but we can use the `config.Config` to pass around the injections
-            target_root: None
-
-            def expand(self,
-                pcoll: beam.PCollection[Tuple[Index, xr.Dataset]],
-            ) ->  beam.PCollection[Tuple[Index, xr.Dataset]]:
-                return pcoll
-
-        config = Config()
-        recipe = (beam.Create() | MyCustomTransform(target_storage=config.target_storage))
-        ```
+    """a class that contains all dependency injections needed for transforms
     """
 
     target_storage: Union[str, FSSpecTarget, RequiredAtRuntimeDefault] = field(
