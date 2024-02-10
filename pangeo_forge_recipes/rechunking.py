c17dd3392e00c28f8ac41c4715d9192193ba7b3c
Charles Stern <62192187+cisaacstern@users.noreply.github.com>
2024-02-09 16:01:41 -0800

    factor out _check_fragment_consistency

--- pangeo_forge_recipes/rechunking.py
+++ pangeo_forge_recipes/rechunking.py
@@ -159,6 +159,32 @@ def _invert_meshgrid(*arrays):
     return xi
 
 
+def _check_fragment_consistency(shape, fragments, dims_starts_sizes) -> None:
+    """ """
+
+    total_size = functools.reduce(operator.mul, shape)
+    if len(fragments) != total_size:
+        # this error path is currently untested
+        raise ValueError(
+            "Cannot combine fragments. "
+            f"Expected a hypercube of shape {shape} but got {len(fragments)} fragments."
+        )
+
+    starts_cube = [np.array(item[1]).reshape(shape) for item in dims_starts_sizes]
+    sizes_cube = [np.array(item[2]).reshape(shape) for item in dims_starts_sizes]
+    try:
+        # reversing order is necessary here because _sort_by_speed_of_varying puts the
+        # arrays into the opposite order as wanted by np.meshgrid
+        starts = _invert_meshgrid(*starts_cube[::-1])[::-1]
+        sizes = _invert_meshgrid(*sizes_cube[::-1])[::-1]
+    except AssertionError:
+        raise ValueError("Cannot combine fragments because they do not form a regular hypercube.")
+
+    expected_sizes = [np.diff(s) for s in starts]
+    if not all(np.equal(s[:-1], es).all() for s, es in zip(sizes, expected_sizes)):
+        raise ValueError(f"Dataset {sizes} and index starts {starts} are not consistent.")
+
+
 # TODO: figure out a type hint that beam likes
 def combine_fragments(
     # FIXME: Julius comment (charles agrees!), rename `group` to be more descriptive,
@@ -224,27 +250,7 @@ def combine_fragments(
 
     shape = [len(np.unique(item[1])) for item in dims_starts_sizes]
 
-    total_size = functools.reduce(operator.mul, shape)
-    if len(fragments) != total_size:
-        # this error path is currently untested
-        raise ValueError(
-            "Cannot combine fragments. "
-            f"Expected a hypercube of shape {shape} but got {len(fragments)} fragments."
-        )
-
-    starts_cube = [np.array(item[1]).reshape(shape) for item in dims_starts_sizes]
-    sizes_cube = [np.array(item[2]).reshape(shape) for item in dims_starts_sizes]
-    try:
-        # reversing order is necessary here because _sort_by_speed_of_varying puts the
-        # arrays into the opposite order as wanted by np.meshgrid
-        starts = _invert_meshgrid(*starts_cube[::-1])[::-1]
-        sizes = _invert_meshgrid(*sizes_cube[::-1])[::-1]
-    except AssertionError:
-        raise ValueError("Cannot combine fragments because they do not form a regular hypercube.")
-
-    expected_sizes = [np.diff(s) for s in starts]
-    if not all(np.equal(s[:-1], es).all() for s, es in zip(sizes, expected_sizes)):
-        raise ValueError(f"Dataset {sizes} and index starts {starts} are not consistent.")
+    _check_fragment_consistency(shape, fragments, dims_starts_sizes)
 
     # some tricky workarounds to put xarray datasets into a nested list
     all_datasets = np.empty(shape, dtype="O").ravel()
