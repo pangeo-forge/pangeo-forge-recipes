7d957a68ea89678bd0b8cb6eb4db0bcefb9e880c
Raphael Hagen <norlandrhagen@gmail.com>
2024-03-13 15:19:21 -0600

    no consolidate

--- pangeo_forge_recipes/transforms.py
+++ pangeo_forge_recipes/transforms.py
@@ -807,12 +807,12 @@ class StoreToPyramid(beam.PTransform, ZarrWriterMixin):
         # To consolidate the top level metadata, we need all the pyramid groups to be written.
         # We are collecting all the pyramid level paths, doing a global combine to fake an AWAIT call,
         # then consolidating the metadata
-        consolidated_path = (
-            transform_pyr_lvls
-            | beam.Flatten()
-            | beam.combiners.Sample.FixedSizeGlobally(1)
-            | beam.Map(lambda x: target_path)
-            | ConsolidateMetadata()
-        )
+        # consolidated_path = (
+        #     transform_pyr_lvls
+        #     | beam.Flatten()
+        #     | beam.combiners.Sample.FixedSizeGlobally(1)
+        #     | beam.Map(lambda x: target_path)
+        #     | ConsolidateMetadata()
+        # )
 
-        return consolidated_path
+        # return consolidated_path
