082b5f8ebfbeae8d0ba308971bce6505b4a19e89
ranchodeluxe <greg.corradini@gmail.com>
2024-03-20 15:37:09 -0700

    wat

--- pangeo_forge_recipes/transforms.py
+++ pangeo_forge_recipes/transforms.py
@@ -767,7 +767,6 @@ class StoreToZarrUgly(beam.PTransform, ZarrWriterMixin):
         index, src_ds_meta_closed = item
         # TODO: deal with multiple combine_dims
         fsspec_kwargs, xarray_kwargs = {}, {}
-        logger.warning(f"FUUUUUCK: {dimension_name} {index.find_filepath(dimension_name)}")
         with fsspec.open(index.find_filepath(dimension_name), mode="rb", **fsspec_kwargs) as open_fs:
             src_ds = xr.open_dataset(open_fs, mode="rb", **xarray_kwargs)
             item = (index, src_ds)
