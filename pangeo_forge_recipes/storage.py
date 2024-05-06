f228e8f0292c807c194c7f1d8e392a289f808dff
sudobangbang <gregcorradini@gmail.com>
2024-05-06 12:28:01 -0700

    more caching disabled

--- pangeo_forge_recipes/storage.py
+++ pangeo_forge_recipes/storage.py
@@ -235,7 +235,7 @@ def _get_opener(fname, secrets, **open_kwargs):
     import s3fs
     fname = fname if not secrets else _add_query_string_secrets(fname, secrets)
     #return fsspec.open(fname, mode="rb", **open_kwargs)
-    fs = s3fs.S3SyncFileSystem(skip_instance_cache=True)
+    fs = s3fs.S3SyncFileSystem(skip_instance_cache=True, use_listings_cache=True)
     open_kwargs.update({"cache_type": "none"})
     return fs.open(fname, mode="rb", **open_kwargs)
 
